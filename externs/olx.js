18352787752c993f3c6506ca997296dcc3779539
richstoner <stonerri@gmail.com>
2014-09-04 15:57:25 -0700

    overmap exposure

--- externs/olx.js
+++ externs/olx.js
@@ -6092,7 +6092,100 @@ olx.source.BiolucidaOptions.prototype.size;
 
 
 
+/**
+ * @typedef {{map: (ol.Map|undefined),
+ *     target: (Element|undefined),
+ *     layers: (Array.<ol.layer.Layer>|ol.Collection|undefined),
+ *     maximized: (boolean|undefined),
+ *     maxRatio: (number|undefined),
+ *     minRatio: (number|undefined),
+ *     rotateBox: (boolean|undefined)}}
+ */
+olx.control.OverviewMapOptions;
 
 
+/**
+ * The map that this overview map is for.
+ * @type {ol.Map|undefined}
+ */
+olx.control.OverviewMapOptions.prototype.map;
+
+
+/**
+ * The target element for this overview map.
+ * @type {Element|undefined}
+ */
+olx.control.OverviewMapOptions.prototype.target;
+
+
+/**
+ * Layers for the overview map. If not set, then all main map layers are used
+ * instead.
+ * @type {Array.<ol.layer.Layer>|ol.Collection|undefined} 
+ */
+olx.control.OverviewMapOptions.prototype.layers;
+
+
+/**
+ * Start as maximized (visible). Defaults to false.
+ * @type {boolean|undefined}
+ */
+olx.control.OverviewMapOptions.prototype.maximized;
+
+
+/**
+ * Maximum extent ratio.
+ * @type {number|undefined}
+ */
+olx.control.OverviewMapOptions.prototype.maxRatio;
+
+
+/**
+ * Minimum extent ratio.
+ * @type {number|undefined}
+ */
+olx.control.OverviewMapOptions.prototype.minRatio;
+
+
+/**
+ * Whether to rotate the box when the main map rotation changes or the overview
+ * map instead. Defaults to false.
+ * @type {boolean|undefined}
+ */
+olx.control.OverviewMapOptions.prototype.rotateBox;
+
+
+
+/**
+ * @typedef {{className: (string|undefined),
+ *     maxResolution: (number|undefined),
+ *     minResolution: (number|undefined)}}
+ * @api stable
+ */
+olx.control.ImageDepthControlOptions;
+
+
+/**
+ * CSS class name.
+ * @type {string|undefined}
+ * @api stable
+ */
+olx.control.ImageDepthControlOptions.prototype.className;
+
+
+/**
+ * Maximum resolution.
+ * @type {number|undefined}
+ * @api stable
+ */
+olx.control.ImageDepthControlOptions.prototype.maxResolution;
+
+
+/**
+ * Minimum resolution.
+ * @type {number|undefined}
+ * @api stable
+ */
+olx.control.ImageDepthControlOptions.prototype.minResolution;
 
 
